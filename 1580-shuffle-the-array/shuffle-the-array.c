5764b83e326c257484abe1a0ee1712c8686296e3
Jawahar Bharathi C <161685706+Jawahar08@users.noreply.github.com>
2026-09-05 22:12:43 +0530

    Time: 13 ms (52.24%) | Memory: 12.2 MB (66.91%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.c
@@ -0,0 +1,16 @@
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* shuffle(int* nums, int numsSize, int n, int* returnSize){
+    int* arr=malloc(numsSize*sizeof(int));
+    *returnSize=numsSize;
+    int j=n;
+    for(int i=0;i<n;i++){
+        arr[2*i]=nums[i];
+        arr[2*i+1]=nums[j];
+        j++;
+    }
+    return arr;
+}
