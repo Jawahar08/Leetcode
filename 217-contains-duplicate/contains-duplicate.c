54be670b8bec25b8eb638eb0c1415731774c8971
Jawahar Bharathi C <161685706+Jawahar08@users.noreply.github.com>
2026-09-06 14:01:13 +0530

    Time: 64 ms (15.70%) | Memory: 19.3 MB (21.07%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.c
@@ -0,0 +1,26 @@
+int compare(const void* a,const void* b){
+    int x=*(int*)a;
+    int y=*(int*)b;
+    if(x<y){
+        return -1;
+    }
+    else if(x>y){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums,numsSize,sizeof(int),compare);
+    for(int i=1;i<numsSize;i++){
+        
+            if(nums[i]==nums[i-1]){
+                return true;
+            }
+        
+
+    }
+    return false;
+}
